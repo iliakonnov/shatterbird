51e69c2f93c60cd6162e249d72402021b6ebd957
Ilia Konnov <ilia@sldr.xyz>
2024-04-28 17:34:41 +0300

    Add frontend

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# vscode-based frontend
+
+See <https://github.com/Felx-B/vscode-web/tree/main/sample> for more information on this.
+
+- src/manifest.json is <https://code.visualstudio.com/api/references/extension-manifest>
+- 
---
+++ frontend/vite.config.ts
@@ -0,0 +1,40 @@
+import {defineConfig} from "vite";
+import {viteStaticCopy} from "vite-plugin-static-copy";
+import path from "path";
+
+export default defineConfig({
+    server: {
+        hmr: {
+            overlay: false,
+        }
+    },
+    build: {
+        lib: {
+            entry: "./src/extension.ts",
+            formats: ["cjs"],
+            fileName: (format, name) => `shatterbird/${name}.js`,
+        },
+        rollupOptions: {
+            external: ["vscode"],
+        },
+        sourcemap: true,
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: path.resolve(__dirname, './src/manifest.json'),
+                    rename: 'package.json',
+                    dest: './shatterbird',
+                },
+                {
+                    src: path.resolve(__dirname, './node_modules/vscode-web/dist') + '/[!.]*',
+                    dest: './vscode-web',
+                }
+            ],
+            watch: {
+                reloadPageOnChange: true
+            }
+        }),
+    ]
+});
